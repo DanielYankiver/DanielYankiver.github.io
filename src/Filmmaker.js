89b8d2dd05590dbb47e10bfb025ad638672c7607
Daniel <dyankiver@gmail.com>
2021-04-01 11:58:27 -0400

    Filmmaker pg: added reel, buttons to video genre pages, styled

--- src/Filmmaker.js
+++ src/Filmmaker.js
@@ -4,8 +4,37 @@ import React from "react";
 function Filmmaker() {
     
     return (
-        <h1>Filmmaker Page</h1>
+        <div>
+            <br/>
+            <br/>
+            <iframe
+                className="reel" 
+                src="https://player.vimeo.com/video/375538495" 
+                width="480" 
+                height="260" 
+                frameborder="0" 
+                allow="autoplay; fullscreen" 
+                allowfullscreen
+            >
+            </iframe>
+            <div classname="video-btns">
+                <br/>
+                <button className="videos-btn">Featured Videos</button>
+                <button className="videos-btn">Commercials</button>
+                <button className="videos-btn">Music Videos</button>
+                <button className="videos-btn">Dance Films</button>
+                <button className="videos-btn">Film Trailers</button>
+                <button className="videos-btn">Short Films</button>
+                <button className="videos-btn">Experimental</button>
+                <button className="videos-btn">Animation</button>
+                <button className="videos-btn">Documentaries</button>
+                <button className="videos-btn">Promos & Clips</button>
+                <button className="videos-btn">Showreels</button>
+                <button className="videos-btn">Behind the Scenes</button>
+            </div>
+        </div>
     );
 }
 
 export default Filmmaker;
+
